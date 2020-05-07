1e0913fc7b5d5055a0a8671039a82cf213c25a86
elprs <eleni.parisi.sunshine@gmail.com>
2020-05-07 21:42:04 +0300

    First Draft Of Order Class

--- Vegan.Entities/DomainClasses/Order.cs
+++ Vegan.Entities/DomainClasses/Order.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Vegan.Entities.Library;
 
 namespace Vegan.Entities
 {
@@ -11,15 +12,50 @@ namespace Vegan.Entities
     {
         [Display(Name = "Order ID")]
         public int OrderId { get; set; }
-        public int Product { get; set; }
-        public int Quantity { get; set; }
         [Display(Name = "Order stamp")]
-        public DateTime OrderTime { get; set; }
+        public DateTime OrderStamp { get; set; }
+
+        public class Item
+        {
+            public Product Product { get; set; }
+            public int Quantity { get; set; }
+        }
+        public IEnumerable<Item> Items { get; set; }
+
+        public ApplicationUser User { get; set; }
+
 
         //======== Navigation Property  Relationships with
         // Product -->  many
-
-
         public virtual ICollection<Product> Products { get; set; }
+
+        // ============================================== Constructor ===========================================
+        public Order(DateTime orderStamp, IEnumerable<Item> items, ICollection<Product> products, ApplicationUser user)
+        {
+            this.OrderStamp = orderStamp;
+            this.Items = items;
+            this.Products = products;
+            this.User = user;
+
+
+        }
+       // ============================================== Methods ===============================================
+       public void AddOrderItem(Item item)
+        {
+            //code
+        }
+
+        public void SetAddress(ApplicationUser user)
+        {
+            //code
+        }
+
+        public decimal CalculateTotal(IEnumerable<Item> items)
+        {
+            //code
+            return;
+        }
+
+
     }
 }
