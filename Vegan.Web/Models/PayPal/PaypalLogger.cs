bf2fd623435044e8cb56f2df852db3dea3665bc0
elprs <eleni.parisi.sunshine@gmail.com>
2020-05-11 10:31:34 +0300

    Add Paypal Logger class

---
+++ Vegan.Web/Models/PayPal/PaypalLogger.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.IO;
+
+namespace Vegan.Web.Models.PayPal
+{
+    public class PaypalLogger
+    {
+        public static string LogDirectoryPath = Environment.CurrentDirectory;
+        public static void Log(String messages)
+        {
+            try
+            {
+                StreamWriter strw = new StreamWriter(LogDirectoryPath + "\\PaypalError.log", true);
+                strw.WriteLine(DateTime.Now.ToString("MM/dd/yyyy HH:mm:ss" + "--->" + messages));
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
