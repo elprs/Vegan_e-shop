75e4eb5d8594c93bfca5fbba5b7a7693a2f99af3
elprs <eleni.parisi.sunshine@gmail.com>
2020-05-22 08:21:28 +0300

    AllProductController

---
+++ Vegan.Web/Controllers/AllProductController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.EnterpriseServices;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vegan.Database;
+using Vegan.Entities.Home;
+using Vegan.Services;
+using Vegan.Web.Models;
+
+namespace Vegan.Web.Controllers
+{
+    public class AllProductController : Controller
+    {
+        private UnitOfWork unitOfWork = new UnitOfWork(new MyDatabase());
+
+        // GET: AllProduct
+        public ActionResult IndexUser()
+        {
+            AllProductViewModel allProductVM = new AllProductViewModel();
+
+            var homes = unitOfWork.Homes.GetAll(); 
+            allProductVM.HomeProducts = (List<Home>)homes;
+
+            return View(allProductVM);
+        }
+    }
+}
---
+++ Vegan.Web/Models/AllProductViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Vegan.Entities.Home;
+using Vegan.Entities.FoodHerb;
+using Vegan.Entities.Care;
+using Vegan.Entities.Supplement;
+using Vegan.Services.HomeRepository;
+using Vegan.Database;
+using Vegan.Services;
+
+namespace Vegan.Web.Models
+{
+    public class AllProductViewModel
+    {
+
+        // ================================ Properties =========================================
+        public List<Home> HomeProducts { get; set; }
+        public List<Candle> CandleProducts { get; set; }
+        //public IEnumerable<FoodHerb> FoodHerbProducts { get; set; }
+        //public IEnumerable<Care> CareProducts { get; set; }
+        //public IEnumerable<Supplement> SupplementProducts { get; set; }
+
+
+
+        // ================================ Constructors =======================================
+
+        public AllProductViewModel()
+        {
+            UnitOfWork unitOfWork = new UnitOfWork(new MyDatabase());
+            HomeProducts = unitOfWork.Homes.GetAll().ToList();
+            CandleProducts = unitOfWork.Candles.GetAll().ToList();
+
+        }
+
+    }
+
+}
