574fc6120e446e63f121034aaff799561e6b13b3
elprs <eleni.parisi.sunshine@gmail.com>
2020-05-22 22:38:33 +0300

    add all categories. Still has bugs

--- Vegan.Web/Controllers/AllProductController.cs
+++ Vegan.Web/Controllers/AllProductController.cs
@@ -21,12 +21,12 @@ namespace Vegan.Web.Controllers
         // GET: AllProduct
         public ActionResult IndexUser(string sortOrder, string searchTitle, int? searchminPrice, int? searchmaxPrice, int? page, int? pSize)
         {
-
+            //================================== Viewbags ====================================
             ViewBag.CurrentTitle = searchTitle;
             ViewBag.CurrentMinPrice = searchminPrice;
             ViewBag.CurrentMaxPrice = searchmaxPrice;
             ViewBag.CurrentSortOrder = sortOrder;
-            ViewBag.CurrentpageSize = pSize;
+            ViewBag.CurrentpSize = pSize;
 
 
 
@@ -34,26 +34,36 @@ namespace Vegan.Web.Controllers
             ViewBag.TitleSortParam = String.IsNullOrWhiteSpace(sortOrder) ? "TitleDesc" : "";
             ViewBag.PriceSortParam = sortOrder == "PriceAsc" ? "PriceDesc" : "PriceAsc";
 
+            ViewBag.TitleView = "badge badge-light";
+            ViewBag.PriceView = "badge badge-light";
+
             AllProductViewModel allProductVM = new AllProductViewModel();
 
-            //Get all the eshop categories
+            //============================  Get all the eshop categories ====================
             var homes = unitOfWork.Homes.GetAll();
+            var cares = unitOfWork.Cares.GetAll();
+            var foodHerbs = unitOfWork.FoodHerbs.GetAll();
+            var supplements = unitOfWork.Supplements.GetAll();
+
+            //================================== Sorting ====================================
+
 
             //Sorting by title & price
             switch (sortOrder)
             {
-                case "TitleDesc": homes = homes.OrderByDescending(x => x.Title).ThenBy(x => x.Price); break;
-                case "TitleAsc": homes = homes.OrderBy(x => x.Title).ThenBy(x => x.Price); break;
-                case "PriceDesc": homes = homes.OrderByDescending(x => x.Price); break;
-                case "PriceAsc": homes = homes.OrderBy(x => x.Price).ThenBy(x => x.Title); break;
-                default: homes = homes.OrderBy(x => x.Title).ThenBy(x => x.Price); break;
+                case "TitleDesc": homes = homes.OrderByDescending(x => x.Title).ThenBy(x => x.Price); ViewBag.TitleView = "badge badge-secondary"; break;
+                case "TitleAsc": homes = homes.OrderBy(x => x.Title).ThenBy(x => x.Price); ViewBag.TitleView = "badge badge-secondary"; break;
+                case "PriceDesc": homes = homes.OrderByDescending(x => x.Price); ViewBag.PriceView = "badge badge-secondary"; break;
+                case "PriceAsc": homes = homes.OrderBy(x => x.Price); ViewBag.PriceView = "badge badge-secondary"; break;
+                default: homes = homes.OrderBy(x => x.Title).ThenBy(x => x.Price); ViewBag.TitleView = "badge badge-secondary"; break;
             }
-            // Sorting page number
+            //Pagination
             int pageSize = pSize ?? 3;
             int pageNumber = page ?? 1;
 
 
-            //======================FILTERS===============================
+            //================================== Filters ====================================
+
             //Filtering  Title
             if (!string.IsNullOrWhiteSpace(searchTitle))
             {
@@ -71,15 +81,17 @@ namespace Vegan.Web.Controllers
                 homes = homes.Where(x => x.Price <= searchmaxPrice);
             }
 
+            // Assign the sorting - searching to the viewModel
             allProductVM.HomeProducts = homes.ToPagedList(pageNumber, pageSize);
-            //homes = homes.OrderBy(x => x.Title).ThenBy(x => x.Price);
-            //allProductVM.HomeProducts
+            allProductVM.CareProducts = cares.ToPagedList(pageNumber, pageSize);
+            allProductVM.FoodHerbProducts = foodHerbs.ToPagedList(pageNumber, pageSize);
+            allProductVM.SupplementProducts = supplements.ToPagedList(pageNumber, pageSize);
+        
+
+
             //TODO Xreiazetai kapou Dispose??? 
 
-            //TODO for the other 3 categories
 
-
-            //    return View(homes.ToPagedList(pageNumber, pageSize));
 
             return View(allProductVM);
         }
