1c32a2cc474afea906af54706763949ac5358e62
poul1986 <you@example.com>
2020-05-26 13:16:53 +0300

    Added info to applicationUsers and added users to roles Installed angularjs

---
+++ Vegan.Web/Scripts/Angular/app.js
@@ -0,0 +1,21 @@
+﻿
+
+var app = angular.module('VeganApp', []);
+
+app.controller("ProductController", function ($scope, $http) {
+
+    var url = "product.json"
+
+    FetchData();
+
+    function FetchData() {
+        $http.get(url)
+            .then(function (response) {
+
+                $scope.Products = respone.data[1].products;
+
+            }, function myError(response) {
+                console.log(response);
+            });
+    }
+});
